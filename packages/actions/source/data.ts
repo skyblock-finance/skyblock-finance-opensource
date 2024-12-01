87db87283951d86fa9a957724aa0b63f04419de4
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2024-12-01 22:16:38 +0100

    version(actions@0.0.10): Fix North Stars Export and Schema

--- packages/actions/source/data.ts
+++ packages/actions/source/data.ts
@@ -1,6 +1,8 @@
 import { actions as _bits } from '../data/bits.json'
 import { actions as _bitsCrafts } from '../data/bits-crafts.json'
 import { actions as _gems } from '../data/gems.json'
+import { actions as _northStars } from '../data/north-stars.json'
+import { actions as _northStarsCrafts } from '../data/north-stars-crafts.json'
 import { Action } from './types'
 
 export const bits = _bits as Action[]
@@ -8,4 +10,13 @@ export const bitsCrafts = _bitsCrafts as Action[]
 
 export const gems = _gems as Action[]
 
-export const allActions = [...bits, ...bitsCrafts, ...gems]
+export const northStars = _northStars as Action[]
+export const northStarsCrafts = _northStarsCrafts as Action[]
+
+export const allActions = [
+	...bits,
+	...bitsCrafts,
+	...gems,
+	...northStars,
+	...northStarsCrafts,
+]
--- packages/actions/source/schema.ts
+++ packages/actions/source/schema.ts
@@ -11,7 +11,7 @@ export const actionIoItemSchema = z
 export const actionIoCurrencySchema = z
 	.object({
 		amount: z.number().finite(),
-		id: z.enum(['bit', 'coin', 'gem', 'northStar', 'second', 'usd']),
+		id: z.enum(['bit', 'coin', 'gem', 'north-star', 'second', 'usd']),
 		type: z.literal('currency'),
 	})
 	.strict()
