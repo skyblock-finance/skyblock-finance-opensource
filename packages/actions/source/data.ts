6675e4760a38ece52bdc22e5fd124849bd5037ee
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2024-12-03 19:48:50 +0100

    version(actions@0.1.0): Add Crimson Isle Items

--- packages/actions/source/data.ts
+++ packages/actions/source/data.ts
@@ -1,5 +1,6 @@
 import { actions as _bits } from '../data/bits.json'
 import { actions as _bitsCrafts } from '../data/bits-crafts.json'
+import { actions as _crimsonIsle } from '../data/crimson-isle.json'
 import { actions as _gems } from '../data/gems.json'
 import { actions as _northStars } from '../data/north-stars.json'
 import { actions as _northStarsCrafts } from '../data/north-stars-crafts.json'
@@ -7,6 +8,7 @@ import { Action } from './types'
 
 export const bits = _bits as Action[]
 export const bitsCrafts = _bitsCrafts as Action[]
+export const crimsonIsle = _crimsonIsle as Action[]
 
 export const gems = _gems as Action[]
 
@@ -16,6 +18,7 @@ export const northStarsCrafts = _northStarsCrafts as Action[]
 export const allActions = [
 	...bits,
 	...bitsCrafts,
+	...crimsonIsle,
 	...gems,
 	...northStars,
 	...northStarsCrafts,
