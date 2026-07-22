749a6263a38c385a6aea789dae2107f1237ad79c
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2026-07-22 16:42:20 +0200

    version(actions@0.26.1): Fix Lumina

--- packages/actions/source/data.ts
+++ packages/actions/source/data.ts
@@ -25,6 +25,7 @@ import { actions as _merchantElizabeth } from '../data/merchants/elizabeth.json'
 import { actions as _merchantFishingMerchant } from '../data/merchants/fishing-merchant.json'
 import { actions as _merchantHilda } from '../data/merchants/hilda.json'
 import { actions as _merchantLotusEater } from '../data/merchants/lotus-eater.json'
+import { actions as _merchantLumina } from '../data/merchants/lumina.json'
 import { actions as _merchantMarthos } from '../data/merchants/marthos.json'
 import { actions as _merchantNemo } from '../data/merchants/nemo.json'
 import { actions as _merchantPhillip } from '../data/merchants/phillip.json'
@@ -82,6 +83,7 @@ export const merchantElizabeth = _merchantElizabeth as Action[]
 export const merchantFishingMerchant = _merchantFishingMerchant as Action[]
 export const merchantHilda = _merchantHilda as Action[]
 export const merchantLotusEater = _merchantLotusEater as Action[]
+export const merchantLumina = _merchantLumina as Action[]
 export const merchantMarthos = _merchantMarthos as Action[]
 export const merchantNemo = _merchantNemo as Action[]
 export const merchantPhillip = _merchantPhillip as Action[]
@@ -148,6 +150,7 @@ export const allActions: Action[] = [
 	...merchantElizabeth,
 	...merchantFishingMerchant,
 	...merchantHilda,
+	...merchantLumina,
 	...merchantLotusEater,
 	...merchantMarthos,
 	...merchantNemo,
